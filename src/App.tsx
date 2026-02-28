98335f901021202ba88f6c0d8b45f91a25059814
Vadim Sinitsa <sinitsavr@gmail.com>
2026-02-28 02:38:12 +0200

    add track details view with lyrics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState, useEffect} from "react";
+import {useState, useEffect} from "react";
 
 function App() {
 
-    const [ selectedTrackId, setSelectedTrackId ] = useState(null)
-    const [ tracks, setTracks ] = useState(null)
+    const [selectedTrackId, setSelectedTrackId] = useState(null)
+    const [tracks, setTracks] = useState(null)
+    const [selectedTrack, setSelectedTrack ] = useState(null)
 
-
-    useEffect(()=> {
+    useEffect(() => {
         console.log('effect')
         fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
             headers: {
@@ -14,48 +14,80 @@ function App() {
             }
         }).then(res => res.json())
             .then(json => setTracks(json.data))
-    },[])
-    if (tracks === null ) {
+
+    }, [])
+    if (tracks === null) {
         return <div>
             <h1>Music Player</h1>
             <span>loading...</span>
         </div>
     }
 
-    if (tracks.length === 0 ) {
+    if (tracks.length === 0) {
         return <div>
-        <h1>Music Player</h1>
-        <span>No Tracks</span>
-    </div>
+            <h1>Music Player</h1>
+            <span>No Tracks</span>
+        </div>
     }
 
-  return (
+    return (
 
-<div>
-    <h1>Music Player</h1>
-    <button onClick={()=>{
-        setSelectedTrackId(null)
-    }}>reset selection</button>
-    <ul>
-        { tracks.map((track)=>{
-            return (
-                <li key={track.id} style={{
-                    border: track.id === selectedTrackId ? '1px solid red' : 'none',
-                }}>
+        <div>
+            <h1>Music Player</h1>
+            <button onClick={() => {
+                setSelectedTrackId(null)
+                setSelectedTrack(null)
+            }}>reset selection
+            </button>
+            <div style={{
+                display: 'flex',
+                gap: "10px"
+            }}>
+                <ul>
+                    {tracks.map((track) => {
 
-                    <div onClick={()=>{
-                        setSelectedTrackId(track.id)}}>
-                        { track.attributes.title }
-                    </div>
-                    <audio src={track.attributes.attachments[0].url} controls></audio>
-                </li>
+                        return (
+                            <li key={track.id} style={{
+                                border: track.id === selectedTrack?.id  ? '1px solid red' : 'none',
+                            }}>
 
-            )}) }
-    </ul>
-</div>
+                                <div onClick={() => {
+                                   // setSelectedTrackId(track.id)
+
+                                    fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks/' + track.id, {
+                                        headers: {
+                                            'api-key': '52fbe20e-f68a-45cb-919e-2ae311ceadfb'
+                                        }
+                                    }).then(res => res.json())
+                                        .then(json => setSelectedTrack(json.data))
+
+                                }}>
+                                    {track.attributes.title}
+                                </div>
+                                <audio src={track.attributes.attachments[0].url} controls></audio>
+                            </li>
+
+                        )
+                    })}
+                </ul>
+                <div>
+                    <h2>Details</h2>
+                    {selectedTrack === null
+                        ? 'Track is not selected'
+                        : <div>
+                            <h3>{selectedTrack.attributes.title }</h3>
+                            <h4>Lyrics</h4>
+                            <p>
+                                {selectedTrack.attributes.lyrics ?? 'No Lyrics'}
+                            </p>
+                        </div>
+                    }
+                </div>
+            </div>
+        </div>
 
 
-  )
+    )
 }
 
 export default App
