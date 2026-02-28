74a868a9f7058f68b672dab4af461496c53d64c5
Vadim Sinitsa <sinitsavr@gmail.com>
2026-02-28 15:34:38 +0200

    Add track details and lyrics section

--- src/App.tsx
+++ src/App.tsx
@@ -72,9 +72,10 @@ function App() {
                 </ul>
                 <div>
                     <h2>Details</h2>
-                    {selectedTrack === null
-                        ? 'Track is not selected'
-                        : <div>
+                    {!selectedTrack &&  !selectedTrackId && 'Track is not selected'}
+                    {!selectedTrack && selectedTrackId && 'Loading...'}
+                    {selectedTrack && selectedTrackId &&  selectedTrack.id !== selectedTrackId && 'Loading...'}
+                    { selectedTrack && <div>
                             <h3>{selectedTrack.attributes.title }</h3>
                             <h4>Lyrics</h4>
                             <p>
